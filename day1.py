652022ffe3dff6762c04be80c8bb01b010e9c09d
Abishek Puri <apuri@connect.ust.hk>
2017-12-01 19:30:56 +0800

    Updated for Part 2 of Day 1

--- day1.py
+++ day1.py
@@ -1,10 +1,11 @@
-def AoC1(input_):
+def AoC1(input_,jumps):
+  # If jumps is 1 then solving Part 2, else solving Part 1
   sum_ = 0
+  steps = 1 if (jumps == 0) else int(len(input_)/2)
   for i,val in enumerate(input_):
-    if i == len(input_) - 1:
-      if(val == input_[0]):
-        sum_ += int(val)
+    if (i >= len(input_) - steps):
+      sum_ += int(val) if (val == input_[0+ i - (len(input_) - steps)]) else 0
     else:
-      if val == input_[i+1]:
-        sum_ += int(val)
-  return sum_
+      sum_ += int(val) if (val == input_[i+steps]) else 0
+  print(sum_)
+  
